7c768ef2e59ef418f330d3e01d9fe4eacf8a7554
Jeferson Sales <jefersonsales1@gmail.com>
2022-04-28 14:56:56 -0300

    created transactios table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Summary from '../Summary'
+import TransactionTable from '../TransactionsTable'
 import { Container } from './styles'
 
 export default function Dashboard() {
   return (
     <Container>
-      <Summary/>
+      <Summary />
+      <TransactionTable />
     </Container>
   )
 }
