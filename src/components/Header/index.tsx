fd5319b9f6a3060072fbb342a79b4fbbb36a86fa
Jeferson Sales <jefersonsales1@gmail.com>
2022-05-02 10:33:16 -0300

    Modal component created. Implemented http verbs and templates for using the MiragejS library and formatting the values displayed in the table

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,14 +4,21 @@ import {
   Content
 } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransitionModal: () => void;
+}
+
+export function Header({ onOpenNewTransitionModal }: HeaderProps) {
+
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransitionModal}>
           Nova transação
         </button>
+
       </Content>
     </Container>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-05-02 09:55:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2022-04-28 10:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transacions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transactions');
+    });
+
+    this.post('/transacions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
 
-
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
   <React.StrictMode>
     <App />
